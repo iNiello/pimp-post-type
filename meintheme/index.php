55c57995941c1147d553044bc12bf974d7f952b5
iNiello <99719850+iNiello@users.noreply.github.com>
2022-06-28 13:39:20 +0200

    wip: #1 fix calass rounded circle

--- meintheme/index.php
+++ meintheme/index.php
@@ -1,7 +1,7 @@
 <!--ce fichier servira de page f'acceuil-->
 
 <!-- récupère le header -->
-<?php get_header()?>
+<?php get_header() ?>
 
 Ici bientôt un slider de ouf!
 
@@ -11,25 +11,37 @@ Début de la boucle pour récupérer tous les articles
 Est-ce qu'il y a des articles 
 
 -->
-<?php if (have_posts()): ?>
-    <ul>
-<!-- Tans qu'il y a des articles-->
-<!-- the_post() récupére ce contenu de ce post -->
-    <?php while (have_posts()): the_post();?>
-        <!-- La fonction the_permalink() est le liens vers l'article -->
-        <li>
-            <a href="<?php the_permalink() ?>"> 
-            <!-- La fonction the_title() récupère le titre de l'article -->
-            <?php the_title() ?>
-            </a>
-        -<?php the_author() ?>/'ID<?php the_ID()?></li>
-    <?php endwhile ?>
-    </ul>
-<!-- S'il n'y a pas d'articles -->
-<?php else: ?>
+<?php if (have_posts()) : ?>
+    <!-- Tans qu'il y a des articles-->
+
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <!-- the_post() récupére ce contenu de ce post -->
+        <?php while (have_posts()) : the_post(); ?>
+            <!-- La fonction the_permalink() est le liens vers l'article -->
+            <div class="col">
+
+                <div class="card h-100 text-center" style=" border-radius: 15px;">
+                    <?php the_post_thumbnail('post-thumbnail', ['class' => ' mx-auto d-block rounded-circle img-fluid card-img-top', 'alt' => 'avatard de profile']); ?>
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
+                        <p class="card-text">
+                            <?php the_content() ?>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <!-- <a href="<?php the_permalink() ?>">Card link</a>
+                                <a href="#" class="card-link">Another link</a> -->
+                        -<?php the_author() ?>/'ID<?php the_ID() ?></li>
+                    </div>
+                </div>
+
+            </div>
+
+        <?php endwhile ?>
+    </div>
+    <!-- S'il n'y a pas d'articles -->
+<?php else : ?>
     <h1>Pas d'articles</h1>
 <?php endif; ?>
 
 
 <!-- récupère le footer -->
-<?php get_footer()?>
+<?php get_footer() ?>
