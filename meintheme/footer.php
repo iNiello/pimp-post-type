7c7621dbf7d8effbed7f725cf039e93039527d24
iNiello <99719850+iNiello@users.noreply.github.com>
2022-07-11 08:26:03 +0200

    wip #1 add menu bootstrap

--- meintheme/footer.php
+++ meintheme/footer.php
@@ -1,4 +1,14 @@
     </div>
+    <footer>
+    <?php wp_nav_menu([
+        //divers arguments comme le nom du menu (theme_location)
+        'theme_location'=> 'footer',
+       //ne pas vouloir le container autour du <ul> de Bootstrap (caontainer false) 
+        'container' => false,
+        // ajouter les classes que l'on souhaite pour ici changer  de celles l'<ul>  de WP
+        'menu_class' => 'navbar-nav mr-auto'
+        ]) ?>
+    </footer>
     <?php wp_footer(); ?>
 </body>
 </html>
--- meintheme/functions.php
+++ meintheme/functions.php
@@ -4,8 +4,17 @@
 function meintheme_mon_titre_de_page_article()
 {
     
-    // récupére le titre de la page ou de l'article
+    // récupére le titre de la page ou de l'article et l'ajoute au theme
     add_theme_support('title-tag');
+    // donne la possibilité de mettre/ajoute au theme une limage en Une de la page ou de l'article
+    add_theme_support( 'post-thumbnails' );
+    // ajoute le menu "menu" dans l'interface du back office, pour fire que notre theme supporte la fonction "menu"
+    add_theme_support( 'menus' );
+    // ajoute les option de choix de menu dans l'interface de l'onglet "menu" 
+    // ce register_nav_menu prend un  id de localisasion ($location) et une description $descition (nom afficher en back office)
+    register_nav_menu('header','En tête du menu');
+    // ce register_nav_menu prend un  id de localisasion ($location) et une description $descition (nom afficher en back office)
+    register_nav_menu('footer', 'Pied de page');
 }
 
 // Mettre le cdn de bootstrap de manière propre
@@ -21,9 +30,28 @@ function meintheme_bootstrap_css_js(){
     wp_enqueue_script('bootstrap');
 }
 
+// ajoute au menu la classe nav-item de bootstrap 
+function meintheme_menu_class($classes)
+{
+    $classes[] = 'nav-item';
+    return $classes;
+}
+
+// ajoute aux liens menu la classe nav-link de bootstrap
+function meintheme_menu_link_class($attrs)
+{
+    $attrs['class'] = 'nav-link';
+    return $attrs;
+}
+
+
 // lorsque cette action 'after_setup_theme' est appelée, je veux que tu me joue la fonction meintheme_mon_titre_de_page_article
 add_action('after_setup_theme','meintheme_mon_titre_de_page_article');
 
 // lorsque cette action 'wp_enqueue_scripts' est appelée, je veux que tu me joue la fonction meintheme_bootstrap_css
 // NE PAS OUBLIER LE "S" DE SCRIPTS
 add_action('wp_enqueue_scripts', 'meintheme_bootstrap_css_js');
+//ce walker nav menu pert d'ajouter des classes qui au menu qui sont dif. des celles générées par wp
+add_filter('nav_menu_css_class', 'meintheme_menu_class');
+//ce walker nav menu pert d'ajouter des classes dif. sur les liens qui sont dans le menu des celles générées par wp
+add_filter('nav_menu_link_attributes', 'meintheme_menu_link_class');
