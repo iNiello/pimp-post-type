414855adf5e5590ba7267f30cebcba7b317efcff
iNiello <99719850+iNiello@users.noreply.github.com>
2022-07-13 15:47:10 +0200

    fix: #3 some typo in the coments

--- meintheme/searchform.php
+++ meintheme/searchform.php
@@ -3,15 +3,14 @@
     theme, WP va prendre l'html d'un searchform par default pour mettre en page mon input
 -->
  <!--
-    esc_url echape les URL et home_URL genère le chemin à partir de la page d'accueil
+    esc_url échappe les URL et home_URL genère le chemin à partir de la page d'accueil
 -->    
 <form class="d-flex" action="<?php echo esc_url(home_url('/')) ?>">
     <!--
-      la fonction get_search_query() eschape nativement (esc_attr()) des attributs (le texte tapé)). 
+      la fonction get_search_query() échappe nativement (esc_attr()) des attributs (le texte,terme tapé). 
     -->
-
     <!-- 
-        le champs qui permet de taper la recherche (<input>) doir avoir la classe "name" et la valeur "s" comme attributes
+        le champ qui permet de taper la recherche (<input>) doir avoir la classe "name" et la valeur "s" comme attributs
         si non la recherce n'est pas renvoyé à l'utilisateur 
     -->
     <input class="form-control me-2" name="s" type="search" placeholder="Rechercher sur le site" aria-label="Search" value="<?php echo get_search_query();?>">
